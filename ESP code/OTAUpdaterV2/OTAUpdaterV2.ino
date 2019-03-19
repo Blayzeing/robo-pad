c100c2512da36ef018e8e58f2bdd818aaa1fe410
Blayze Millward <blayze.millward@googlemail.com>
2019-03-19 12:18:19 +0000

    Updated ESP code

--- ESP code/OTAUpdaterV2/OTAUpdaterV2.ino
+++ ESP code/OTAUpdaterV2/OTAUpdaterV2.ino
@@ -7,8 +7,8 @@
 const char* ssid = "robo-padd";
 const char* password = "roboteers";
 
-//ESP8266WebServer httpServer(80);
-//ESP8266HTTPUpdateServer httpUpdater(true);
+ESP8266WebServer httpServer(80);
+ESP8266HTTPUpdateServer httpUpdater(true);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
 unsigned long lastTime = 0;
@@ -22,13 +22,13 @@ void setup(void){
 
   WiFi.softAP(ssid, password);
 
-  //// Setting up the updater
-  //httpUpdater.setup(&httpServer);
+  // Setting up the updater
+  httpUpdater.setup(&httpServer);
 
-  //// Setting up the websocket controller
-  //httpServer.on("/controller", handleController);
+  // Setting up the websocket controller
+  httpServer.on("/controller", handleController);
 
-  //httpServer.begin();
+  httpServer.begin();
 
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("HTTPUpdateServer ready! Open http://");
@@ -46,59 +46,59 @@ void loop(void){
   //  lastTime = millis();
   //  Serial.println("hi");
   //}
-  //httpServer.handleClient();
+  httpServer.handleClient();
   webSocket.loop();
 }
 
-//void handleController() {
-//  String page = 
-//           "<html>\n\
-//            <head>\n\
-//              <!--<meta http-equiv='refresh' content='5'/>!-->\n\
-//              <title>ESP8266 Demo</title>\n\
-//              <style>\n\
-//                body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\n\
-//              </style>\n\
-//              <script>\n\
-//                var ledState = false;\n\
-//                var theInterval;\n\
-//                function init(){\n\
-//                  console.log('init complete...');\n\
-//                  var testSocket = new WebSocket(\"ws://192.168.4.1:81\");\n\
-//                  console.log('connection attempted...');\n\
-//                  testSocket.onopen = function (event) {\n\
-//                    console.log('Sending data...');\n\
-//                    testSocket.send(\"WE'RE IN.\");\n\
-//                    // Shutdown commands.\n\
-//                    document.getElementById(\"disconnectBtn\").onclick=function(){\n\
-//                      testSocket.close();\n\
-//                      //clearInterval(theInterval);\n\
-//                    };\n\
-//                    //// Control commands.\n\
-//                    //document.getElementById(\"LEDBtn\").onclick=function(){\n\
-//                    //  ledState = !ledState;\n\
-//                    //}\n\
-//                    console.log('setting interval...');\n\
-//                    theInterval = setInterval(function(){console.log('sending data...'); testSocket.send(\"some data\");},1000);\n\
-//                  };\n\
-//                }\n\
-//                //function sendData(){\n\
-//                //  // Should be an ArrayBuffer\n\
-//                //  testSocet.send(ledState?\"ledon\":\"ledoff\");\n\
-//                //}\n\
-//                if(document.getElementById) window.onload=init;\n\
-//              </script>\n\
-//            </head>\n\
-//            <body>\n\
-//              <h1>This is the websocket page</h1>\n\
-//              <p>Bleh.</p>\n\
-//              <input id=\"disconnectBtn\" type=\"button\" value=\"Disconnect\"/>\n\
-//              <input id=\"LEDBtn\" type=\"button\" value=\"LED\"/>\n\
-//            </body>\n\
-//          </html>";
-//
-//  httpServer.send(200, "text/html", page);
-//}
+void handleController() {
+  String page = 
+           "<html>\n\
+            <head>\n\
+              <!--<meta http-equiv='refresh' content='5'/>!-->\n\
+              <title>ESP8266 Demo</title>\n\
+              <style>\n\
+                body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\n\
+              </style>\n\
+              <script>\n\
+                var ledState = false;\n\
+                var theInterval;\n\
+                function init(){\n\
+                  console.log('init complete...');\n\
+                  var testSocket = new WebSocket(\"ws://192.168.4.1:81\");\n\
+                  console.log('connection attempted...');\n\
+                  testSocket.onopen = function (event) {\n\
+                    console.log('Sending data...');\n\
+                    testSocket.send(\"WE'RE IN.\");\n\
+                    // Shutdown commands.\n\
+                    document.getElementById(\"disconnectBtn\").onclick=function(){\n\
+                      testSocket.close();\n\
+                      //clearInterval(theInterval);\n\
+                    };\n\
+                    // Control commands.\n\
+                    document.getElementById(\"LEDBtn\").onclick=function(){\n\
+                      ledState = !ledState;\n\
+                    }\n\
+                    console.log('setting interval...');\n\
+                    theInterval = setInterval(function(){console.log('sending data...'); sendData();},500);\n\
+                    function sendData(){\n\
+                      // Should be an ArrayBuffer\n\
+                      testSocket.send(ledState?\"ledon\":\"ledoff\");\n\
+                    }\n\
+                  };\n\
+                }\n\
+                if(document.getElementById) window.onload=init;\n\
+              </script>\n\
+            </head>\n\
+            <body>\n\
+              <h1>This is the websocket page</h1>\n\
+              <p>Bleh.</p>\n\
+              <input id=\"disconnectBtn\" type=\"button\" value=\"Disconnect\"/>\n\
+              <input id=\"LEDBtn\" type=\"button\" value=\"LED\"/>\n\
+            </body>\n\
+          </html>";
+
+  httpServer.send(200, "text/html", page);
+}
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
 
