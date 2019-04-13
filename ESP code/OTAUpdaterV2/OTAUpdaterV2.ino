d93177ee5dbdd409d9d50b12ac3b5d02ef66deb1
Blayze Millward <blayze.millward@googlemail.com>
2019-04-13 17:55:57 +0100

    Now sends ArrayBuffers from the robo

--- ESP code/OTAUpdaterV2/OTAUpdaterV2.ino
+++ ESP code/OTAUpdaterV2/OTAUpdaterV2.ino
@@ -4,6 +4,19 @@
 #include <ESP8266HTTPUpdateServer.h>
 #include <WebSocketsServer.h>
 
+#include "ControllerCode.h"
+//#include <OTAUpdaterV2.h>
+
+//TODO:
+// - Try MDNS
+// - JS generator minification
+// -- minification
+// -- comment removal
+// -- 
+// - Stop the robot from doing anything else while it's updating (does it already do this?)
+// - Define the ArrayBuffer contents dynamically, configure which bits mean what
+// -- Should probably be the minimum length of the data being sent, aka bools use 1 bit, drive commands 1 byte.
+
 const char* ssid = "robo-padd";
 const char* password = "roboteers";
 
@@ -41,63 +54,17 @@ void setup(void){
 }
 
 void loop(void){
-  //if(millis() - lastTime > 60000)
-  //{
-  //  lastTime = millis();
-  //  Serial.println("hi");
-  //}
+  if(millis() - lastTime > 6000)
+  {
+    lastTime = millis();
+    Serial.println("hi");
+  }
   httpServer.handleClient();
   webSocket.loop();
 }
 
 void handleController() {
-  String page = 
-           "<html>\n\
-            <head>\n\
-              <!--<meta http-equiv='refresh' content='5'/>!-->\n\
-              <title>ESP8266 Demo</title>\n\
-              <style>\n\
-                body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\n\
-              </style>\n\
-              <script>\n\
-                var ledState = false;\n\
-                var theInterval;\n\
-                function init(){\n\
-                  console.log('init complete...');\n\
-                  var testSocket = new WebSocket(\"ws://192.168.4.1:81\");\n\
-                  console.log('connection attempted...');\n\
-                  testSocket.onopen = function (event) {\n\
-                    console.log('Sending data...');\n\
-                    testSocket.send(\"WE'RE IN.\");\n\
-                    // Shutdown commands.\n\
-                    document.getElementById(\"disconnectBtn\").onclick=function(){\n\
-                      testSocket.close();\n\
-                      //clearInterval(theInterval);\n\
-                    };\n\
-                    // Control commands.\n\
-                    document.getElementById(\"LEDBtn\").onclick=function(){\n\
-                      ledState = !ledState;\n\
-                    }\n\
-                    console.log('setting interval...');\n\
-                    theInterval = setInterval(function(){console.log('sending data...'); sendData();},500);\n\
-                    function sendData(){\n\
-                      // Should be an ArrayBuffer\n\
-                      testSocket.send(ledState?\"ledon\":\"ledoff\");\n\
-                    }\n\
-                  };\n\
-                }\n\
-                if(document.getElementById) window.onload=init;\n\
-              </script>\n\
-            </head>\n\
-            <body>\n\
-              <h1>This is the websocket page</h1>\n\
-              <p>Bleh.</p>\n\
-              <input id=\"disconnectBtn\" type=\"button\" value=\"Disconnect\"/>\n\
-              <input id=\"LEDBtn\" type=\"button\" value=\"LED\"/>\n\
-            </body>\n\
-          </html>";
-
-  httpServer.send(200, "text/html", page);
+  httpServer.send(200, "text/html", PAGE_HTML);
 }
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
@@ -120,6 +87,8 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
     case WStype_TEXT:
       Serial.printf("[%u] get Text: %s\n", num, payload);
 
+      //if(payload
+
       // send message to client
       // webSocket.sendTXT(num, "message here");
 
@@ -128,6 +97,8 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
       break;
     case WStype_BIN:
       Serial.printf("[%u] get binary length: %u\n", num, length);
+      Serial.printf("[%u] Data[0]: %u\n", num, payload[0]);
+      Serial.printf("[%u] Data[1]: %u\n", num, payload[1]);
       hexdump(payload, length);
 
       // send message to client
