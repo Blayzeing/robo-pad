ce613965e7f58f612a3891aad243261101f0dffa
Blayze Millward <blayze.millward@googlemail.com>
2020-01-17 18:23:10 +0000

    Updates version number

--- Firmware/RoboPad/RoboPad.ino
+++ Firmware/RoboPad/RoboPad.ino
@@ -16,7 +16,7 @@
 
 const char* ssid = "robo-pad";
 const char* password = "roboteers";
-const char* VERSION_NUMBER = "1.4a";
+const char* VERSION_NUMBER = "1.4b";
 const char* SAFEBOOT_HTML = "ROBOPAD IS IN SAFEBOOT MODE, CLICK <a href='update'>HERE</a> TO UPLOAD NEW FIRMWARE.";
 const char* HOME_HTML_FORMAT = "version: %s<br><a href='controller'>Dev Page</a><br><a href='newController'>Web Controller</a><br><a href='spinner'>Spinner Controller</a><br><a href='update'>updater</a>\0";
 char home_html[200];
