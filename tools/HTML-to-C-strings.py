5b9f4cfc358bf1f5c7e3c9596cd3726a03c355d2
Blayze Millward <blayze.millward@googlemail.com>
2020-07-13 00:07:26 +0100

    Handles all enclosures but string literals

--- tools/HTML-to-C-strings.py
+++ tools/HTML-to-C-strings.py
@@ -20,14 +20,17 @@ from functools import reduce
 from enum import Enum
 
 class State(Enum):
-  IN_HTML               = 1
-  IN_HTML_COMMENT       = 2
-  IN_JS                 = 3
-  IN_JS_BLOCK_COMMENT   = 4
-  IN_JS_STRING_LITERAL  = 5
-  IN_CSS                = 6
-  IN_CSS_BLOCK_COMMENT  = 7
-  IN_CSS_STRING_LITERAL = 8
+  IN_HTML                   = 1
+  IN_HTML_COMMENT           = 2
+  IN_JS                     = 3
+  IN_JS_SINGLE_LINE_COMMENT = 4
+  IN_JS_BLOCK_COMMENT       = 5
+  IN_JS_STRING_LITERAL1     = 6  # '
+  IN_JS_STRING_LITERAL2     = 7  # "
+  IN_CSS                    = 8
+  IN_CSS_BLOCK_COMMENT      = 9
+  IN_CSS_STRING_LITERAL1    = 10 # '
+  IN_CSS_STRING_LITERAL2    = 11 # "
 
 def main(argv):
   USAGE_STRING = "USAGE:\n\nHTML-to-C-strings.y -i <HTML folder path (defaults to '../HTML/'> [-o <output header file name (defaults to 'htmlStrings.h')>]\n"
@@ -66,45 +69,191 @@ def main(argv):
 # Ingests the file and gets rid of unwanted characters
 def removeVerbosity(filepath):
   output = ""
-  State state = State.IN_HTML # state variable to track what sort of tokens we'll be parsing
-  for line in htmlFile:
-    result, state = parseString(line, state)
-    output = output + result
+  state = State.IN_HTML # state variable to track what sort of tokens we'll be parsing
+  with open(filepath) as infile:
+    for line in infile:
+      result, state = parseLine(line, state)
+      #print("RESULT: ", state, result)
+      if result.strip() != "":
+        output = output + result.strip() + "\n"
+    print(output)
+
+RESERVED_WORDS = [] # A list of reserved words from HTML and JS here (See: https://www.w3schools.in/javascript-tutorial/keywords/ )
+
+# The state transition table, in the form: [<Current State>][<string found>][<next state>]
+BASE_TRANSITION_TABLE = { State.IN_HTML: {"<!--" : State.IN_HTML_COMMENT,
+                                    "<script>" : State.IN_JS,
+                                    "<style>" : State.IN_CSS},
+                     State.IN_HTML_COMMENT: {"-->" : State.IN_HTML},
+                     State.IN_JS: {"</script>" : State.IN_HTML,
+                                   "/*" : State.IN_JS_BLOCK_COMMENT,
+                                   "'" : State.IN_JS_STRING_LITERAL1,
+                                   "\"" : State.IN_JS_STRING_LITERAL2,
+                                   "//" : State.IN_JS_SINGLE_LINE_COMMENT},
+                     State.IN_JS_SINGLE_LINE_COMMENT: {"\n" : State.IN_JS},
+                     State.IN_JS_BLOCK_COMMENT: {"*/" : State.IN_JS},
+                     State.IN_JS_STRING_LITERAL1: {"[^\]'": State.IN_JS},
+                     State.IN_JS_STRING_LITERAL2: {"[^\}\"": State.IN_JS},
+                     State.IN_CSS: {"</style>" : State.IN_HTML,
+                                    "/*" : State.IN_CSS_BLOCK_COMMENT,
+                                    "'" : State.IN_CSS_STRING_LITERAL1,
+                                    "\"" : State.IN_CSS_STRING_LITERAL2},
+                     State.IN_CSS_BLOCK_COMMENT: {"*/" : State.IN_CSS},
+                     State.IN_CSS_STRING_LITERAL1: {"[^\]'" : State.IN_CSS},
+                     State.IN_CSS_STRING_LITERAL2: {"[^\]\"" : State.IN_CSS}
+                    }
+STYLE_TRANSITION_TABLE = { State.IN_HTML: { "<style>" : State.IN_CSS},
+                     State.IN_HTML_COMMENT: {},
+                     State.IN_JS: {},
+                     State.IN_JS_BLOCK_COMMENT: {},
+                     State.IN_JS_SINGLE_LINE_COMMENT: {"\n" : State.IN_JS},
+                     State.IN_JS_STRING_LITERAL1: {},
+                     State.IN_JS_STRING_LITERAL2: {},
+                     State.IN_CSS: {"</style>" : State.IN_HTML},
+                     State.IN_CSS_BLOCK_COMMENT: {},
+                     State.IN_CSS_STRING_LITERAL1: {},
+                     State.IN_CSS_STRING_LITERAL2: {}
+                    }
+EXTENDED_TRANSITION_TABLE = { State.IN_HTML: {"<!--" : State.IN_HTML_COMMENT,
+                                    "<script>" : State.IN_JS,
+                                    "<style>" : State.IN_CSS},
+                     State.IN_HTML_COMMENT: {"-->" : State.IN_HTML},
+                     State.IN_JS: {"</script>" : State.IN_HTML,
+                                   "/*" : State.IN_JS_BLOCK_COMMENT,
+                                   "//" : State.IN_JS_SINGLE_LINE_COMMENT},
+                     State.IN_JS_SINGLE_LINE_COMMENT: {"\n" : State.IN_JS},
+                     State.IN_JS_BLOCK_COMMENT: {"*/" : State.IN_JS},
+                     State.IN_JS_STRING_LITERAL1: {},
+                     State.IN_JS_STRING_LITERAL2: {},
+                     State.IN_CSS: {"</style>" : State.IN_HTML,
+                                    "/*" : State.IN_CSS_BLOCK_COMMENT},
+                     State.IN_CSS_BLOCK_COMMENT: {"*/" : State.IN_CSS},
+                     State.IN_CSS_STRING_LITERAL1: {},
+                     State.IN_CSS_STRING_LITERAL2: {}
+                    }
+EXTENDED_TRANSITION_TABLE = { State.IN_HTML: {"<!--" : State.IN_HTML_COMMENT,
+                                    "<script>" : State.IN_JS,
+                                    "<style>" : State.IN_CSS},
+                     State.IN_HTML_COMMENT: {"-->" : State.IN_HTML},
+                     State.IN_JS: {"</script>" : State.IN_HTML},
+                     State.IN_JS_SINGLE_LINE_COMMENT: {"\n" : State.IN_JS},
+                     State.IN_JS_BLOCK_COMMENT: {},
+                     State.IN_JS_STRING_LITERAL1: {},
+                     State.IN_JS_STRING_LITERAL2: {},
+                     State.IN_CSS: {"</style>" : State.IN_HTML},
+                     State.IN_CSS_BLOCK_COMMENT: {},
+                     State.IN_CSS_STRING_LITERAL1: {},
+                     State.IN_CSS_STRING_LITERAL2: {}
+                    }
+NOSTRINGS_TRANSITION_TABLE = { State.IN_HTML: {"<!--" : State.IN_HTML_COMMENT,
+                                    "<script>" : State.IN_JS,
+                                    "<style>" : State.IN_CSS},
+                     State.IN_HTML_COMMENT: {"-->" : State.IN_HTML},
+                     State.IN_JS: {"</script>" : State.IN_HTML,
+                                   "/*" : State.IN_JS_BLOCK_COMMENT,
+                                   "//" : State.IN_JS_SINGLE_LINE_COMMENT},
+                     State.IN_JS_SINGLE_LINE_COMMENT: {"\n" : State.IN_JS},
+                     State.IN_JS_BLOCK_COMMENT: {"*/" : State.IN_JS},
+                     State.IN_JS_STRING_LITERAL1: {},
+                     State.IN_JS_STRING_LITERAL2: {},
+                     State.IN_CSS: {"</style>" : State.IN_HTML,
+                                    "/*" : State.IN_CSS_BLOCK_COMMENT},
+                     State.IN_CSS_BLOCK_COMMENT: {"*/" : State.IN_CSS},
+                     State.IN_CSS_STRING_LITERAL1: {},
+                     State.IN_CSS_STRING_LITERAL2: {}
+                    }
+TRANSITION_TABLE = NOSTRINGS_TRANSITION_TABLE
+COMMENT_STATES = [State.IN_HTML_COMMENT, State.IN_JS_SINGLE_LINE_COMMENT, State.IN_JS_BLOCK_COMMENT, State.IN_CSS_BLOCK_COMMENT]
+
+COMMENT_STRINGS = ["<!--", "-->", "/*", "*/", "//"]
 
 # Parses a string and a state variable, processes the string and returns an updated state and resultant string
-def parseString(code, inState):
+def parseLine(line, inState):
   output = ""
-  outputState = inState
-  if inState == State.IN_HTML:
-    # Check for start of HTML comment, start of JS, start of CSS
-    # Then parse the leftovers
-  elif inState == State.IN_HTML_COMMENT:
-    # Check for end of HTML comment
-    # Parse leftovers
-  elif inState == State.IN_JS:
-    # Remove eol comments
-    line = line.split("//")[0];
-    # Check for end of JS
-    # Parse leftovers
-  elif inState == State.IN_JS_BLOCK_COMMENT:
-    # Check for end of JS block comment
-    # Parse leftovers
-  elif inState == State.IN_JS_STRING_LITERAL:
-    # Check for end of JS string literal
-    # Parse leftovers
-  elif inState == State.IN_CSS:
-    # Check for end of CSS, start of CSS block comment, start of CSS string literal
-    # Parse leftovers
-  elif inState == State.IN_CSS_BLOCK_COMMENT:
-    # Check for end of CSS block comment
-    # Parse leftovers
-  elif inState == State.IN_CSS_STRING_LITERAL:
-    # Check for end of CSS string literal
-    # Parse leftovers
-  else:
-    # Bleh. Not sure what happens here.
-    # Find the first ins
+  outState = inState
 
+  #print("PARSING AS "+str(inState)+": ", line)
+
+  if line != "" and len(TRANSITION_TABLE[inState].keys()) > 0:
+    # Store the indicies of the first of each of the associated potential transition strings in the line:
+    occuranceIndicies = [line.find(k) for k in TRANSITION_TABLE[inState].keys()]
+    # Now zip(? don't. Just keep them both as ordered lists) the occurance indicies with the states that they produce,
+    # Argmin to find the first (if existing) ocuring match, store the state that implies will be next
+    closestIndex, value = argmin(occuranceIndicies, len(line))
+    #print("DATA:", "'"+line.strip()+"'\n", "PROCESSING:", list(TRANSITION_TABLE[inState].keys()), occuranceIndicies, closestIndex, value, "END DATA")
+    nextState = inState # For when the state doesn't change
+    if closestIndex == -1:
+      # None of the strings were found, so set the value to the end of the line
+      value = len(line)
+    else:
+      nextState = TRANSITION_TABLE[inState][list(TRANSITION_TABLE[inState].keys())[closestIndex]]
+      #print("State transition found!", nextState)
+    # Process (depending on state [write/don't write, eliminate spaces etc]) the stuff up until and including (depending on if the previous state or next state is a COMMENT_STATE) the first match
+    leftHalf = line[:value]# The string up to (and excluding the transition string) the transition string
+    tokenString = list(TRANSITION_TABLE[inState].keys())[closestIndex]
+    #rightHalf = line[value + (len(tokenString) if nextState in COMMENT_STATES else 0):]# The remaining half of the line (excluding the transition string if the next state is a comment)
+    rightHalf = line[value + (len(tokenString) if tokenString in COMMENT_STRINGS else 0):]# The remaining half of the line (excluding the transition string if the next state is a comment)
+    
+    if(inState in COMMENT_STATES):
+      # If we're in a comment state, just move on
+      output, outState = parseLine(rightHalf, nextState)
+    else:
+      # If we're not, then perform state-specific masking of the output
+      output = stateSpecificMasking(leftHalf)
+      parsedRight, outState = parseLine(rightHalf, nextState)
+      output = output + parsedRight
+
+  # If the line is empty, return input data as it's reached it's terminal recursion
+  return output, outState
+
+# Takes a tokenless string and alters it based on the contents
+def stateSpecificMasking(line):
+  return line # For now just pass back the entire string. Maybe Do things like variable contraction or operator space removal in the future
+
+# Returns the (index, and then the value of the lowest number in a list):
+#argmin = lambda ls, maxInt: reduce(lambda indexWithTotal, n: n if n[1]<indexWithTotal[1] and n[0] != -1 else indexWithTotal, enumerate(ls), (-1,maxInt))
+# (index in search list, value)
+#argmin = lambda ls, maxInt: reduce(lambda indexWithTotal, n: n if n[1]<indexWithTotal[1] else indexWithTotal, enumerate(ls), (0,maxInt))
+def argmin(ls, mx):
+  best = mx
+  bestI = -1
+  for i, l in enumerate(ls):
+    if l < best and l>=0:
+      best = l
+      bestI = i
+  return bestI, best
+
+#  if inState == State.IN_HTML:
+#    # Check for start of HTML comment, start of JS, start of CSS
+#    # Then parse the leftovers
+#  elif inState == State.IN_HTML_COMMENT:
+#    # Check for end of HTML comment
+#    # Parse leftovers
+#  elif inState == State.IN_JS:
+#    # Remove eol comments
+#    line = line.split("//")[0];
+#    # Check for end of JS
+#    # Parse leftovers
+#  elif inState == State.IN_JS_BLOCK_COMMENT:
+#    # Check for end of JS block comment
+#    # Parse leftovers
+#  elif inState == State.IN_JS_STRING_LITERAL:
+#    # Check for end of JS string literal
+#    # Parse leftovers
+#  elif inState == State.IN_CSS:
+#    # Check for end of CSS, start of CSS block comment, start of CSS string literal
+#    # Parse leftovers
+#  elif inState == State.IN_CSS_BLOCK_COMMENT:
+#    # Check for end of CSS block comment
+#    # Parse leftovers
+#  elif inState == State.IN_CSS_STRING_LITERAL:
+#    # Check for end of CSS string literal
+#    # Parse leftovers
+#  else:
+#    # Bleh. Not sure what happens here.
+#    # Find the first ins
+
+#def searchForFirst(words, <LIST OF STRINGS AND THEIR RELATIVE STATE CHANGES (perhaps this should be a dict?)>)
 
 
 #    # Get rid of comments
@@ -129,10 +278,9 @@ def parseString(code, inState):
 #    print(line, end='')
 
 # Returns whether the position is between these two things within the code. This should probably be tracked line at a time...
-def isSurrounded(start, position?, end):
-  return Eh
+#def isSurrounded(start, position?, end):
+#  return Eh
 
-RESERVED_WORDS = [] # A list of reserved words from HTML and JS here (See: https://www.w3schools.in/javascript-tutorial/keywords/ )
 
 if __name__ == "__main__":
   main(sys.argv[1:])
